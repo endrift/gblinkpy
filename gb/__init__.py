f5ad953c0253ae0e505393bdf2783bbf0638f1df
Vicki Pfau <vi@endrift.com>
2019-08-27 22:28:25 -0700

    Minor parallel fix

--- gb/__init__.py
+++ gb/__init__.py
@@ -5,13 +5,13 @@ class Link:
 
     def __init__(self, p):
         self.p = p
+        self.p.set_bit(self.SC, 1)
 
     def tx(self, byte):
         rx = 0
         for i in range(7, -1, -1):
             bit = (byte >> i) & 1
             self.p.set_bit(self.SO, bit)
-            self.p.set_bit(self.SC, 1)
             self.p.set_bit(self.SC, 0)
             rx |= self.p.get_bit(self.SI) << i
             self.p.set_bit(self.SC, 1)
